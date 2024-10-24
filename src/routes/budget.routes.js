0cd485dc309c0a3346f552028c65a27956c5d693
Salutah <salutahmoha@gmail.com>
2024-10-24 15:58:39 +0300

    refactoring codes for easier readability

--- src/routes/budget.routes.js
+++ src/routes/budget.routes.js
@@ -1,21 +1,27 @@
-// import {Router} from 'express';
+import { Router } from "express";
 
-// import {createBudget, getAllBudgets, getBudget, updateBudget, deleteBudget} from '../controllers/budget.controllers.js';
-// const router = Router();
+import {
+  createBudget,
+  getAllBudgets,
+  getBudget,
+  updateBudget,
+  deleteBudget,
+} from "../controllers/budget.controllers.js";
+const router = Router();
 
-// // create a budget -/budgets
-// router.post('', createBudget)
+// create a budget -/budgets
+router.post("", createBudget);
 
-// // get all budgets - /budgets
-// router.get('', getAllBudgets)
+// get all budgets - /budgets
+router.get("", getAllBudgets);
 
-// // get a single budget - /budgets/:id
-// router.get('/:title', getBudget)
+// get a single budget - /budgets/:id
+router.get("/:title", getBudget);
 
-// // update a budget - /budgets/:id
-// router.patch('/:title', updateBudget)
+// update a budget - /budgets/:id
+router.patch("/:title", updateBudget);
 
-// // delete a budget - /budgets/:id
-// router.delete('/:title', deleteBudget)
+// delete a budget - /budgets/:id
+router.delete("/:title", deleteBudget);
 
-// export default router;
+export default router;
